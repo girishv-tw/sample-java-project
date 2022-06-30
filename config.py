2f41a94c9a8271a67871a2f15c7defbde9fd811d
anuragpathak2608 <48289570+anuragpathak2608@users.noreply.github.com>
2022-06-30 17:02:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,11 @@ app_token = "ajdksjaksdjkajsdkajdkajddsadasdasdasda"
 
 # AWS
 secret_key=R4nm063hgMSADT5xOs5nHLeLXA6lar7ZJ3Nt0j
+
+pass = R4nm063hgMSADT5xOs5
+secret = P@sswo0rd
+key=R4nm063hgMSADT5xOs5nHLeLXA6lar7ZJ3Nt0j
+key = AKIAJWVN5Z4FOFT7NLNB
+AWS = AKIAJWVN5Z4FOFT7NLNB
+pass = AKIAJWVN5Z4FOFT7NLNB
+credsAKIAJWVN5Z4FOFT7NLNB784372
